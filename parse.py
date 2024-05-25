bb21dc9e7b522fab6607eaffa9012ad2873ea07a
David Langat <davidcheruiyot.langat@connect.qut.edu.au>
2024-05-25 12:24:31 +1000

    Change parse.py to match the document collection and doc object

--- parse.py
+++ parse.py
@@ -1,12 +1,13 @@
 import os, string, glob
 from stemming.porter2 import stem
 from bow_doc import BowDoc
+from bow_coll import BowColl
 
 
 
 def parse_rcv1v2(stop_words, inputpath):
+    bow_coll = BowColl()                            #Create an instance of the BowColl class
     #local variables
-    document_collection = {}                      #A dictonary collection of document objects as values and their ids as keys.
     os.chdir(inputpath)                             #change working directory to the inputpath variable
     for file in glob.glob('*.xml'):                 #Iterate through all files with .xml
         document = BowDoc(docid=0)    #Initializing a document object for the current file
@@ -32,7 +33,11 @@ def parse_rcv1v2(stop_words, inputpath):
                     term = stem(term.lower()) 
                     if len(term) > 2 and term not in stop_words:  
                         document.add_term(term)                     # I add terms into the document object. A term is a stem of a word that has more than 2 characters and is not a common english word.
-                        
-        document_collection[document.docId] = document       
+        
+        #set the doc_len of the document object
+        document.set_doc_len(document.doc_len)
+        #add document to the collection object Bowcoll
+        bow_coll.add_doc(document)  # Call the add_doc method on the instance
+
     os.chdir('..')
-    return document_collection
+    return bow_coll
